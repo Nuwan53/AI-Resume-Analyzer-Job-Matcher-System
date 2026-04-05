7ea1e0ae9b16e0f66dd9ba369afa1033b9722f3f
Nuwan Ganganath <nuwanrathnayaka53@gmail.com>
2026-04-05 19:54:42 +0530

    Add compiled URL configuration for resume_ai project

--- resume_ai/analyzer/models.py
+++ resume_ai/analyzer/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Resume(models.Model):
+    file = models.FileField(upload_to = 'resumes/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class ExtarctedData(models.Model):
+    resume = models.OneToOneField(Resume, on_delete=models.CASCADE)
+    skills = models.TextField()
+    experience = models.TextField()
+    education = models.TextField()
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    required_skills = models.TextField()
+
+class MatchResult(models.Model):
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    score = models.FloatField()
+    
